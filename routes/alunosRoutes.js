df00859e55c274a22c3be3912ed8cea5c4ed116f
João Vitor L <86082815+Lima-goncalves@users.noreply.github.com>
2022-06-09 22:45:13 -0300

    Add files via upload

---
+++ routes/alunosRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const AlunoController = require('../controllers/AlunoController')
+
+router.get('/add', AlunoController.createAluno)
+router.post('/add', AlunoController.createAlunoSave)
+router.post('/remove', AlunoController.removeAluno)
+router.get('/edit/:id', AlunoController.updateAluno)
+router.post('/edit', AlunoController.updateAlunoPost)
+router.get('/', AlunoController.showAlunos)
+
+router.post('/updatestatus', AlunoController.changeStatus)
+
+module.exports = router
